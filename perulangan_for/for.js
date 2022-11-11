176a79fa7ad5d60afcf60a95fb2190258d17b01d
damosdmahdi <godrihidayat2@gmail.com>
2022-11-11 11:15:27 +0700

    perulangan for

---
+++ perulangan_for/for.js
@@ -0,0 +1,15 @@
+/*for (var Toko = 1; Toko <= 10; Toko++) {
+  console.log("Toko bekerja dengan lancar adalah toko " + Toko);
+}*/
+
+// Studi kasus
+
+for (toko = 1; toko <= 6; toko++) {
+  console.log("Toko bekerja dengan lancar adalah toko " + toko);
+}
+
+var toko = 7;
+while (toko <= 10) {
+  console.log("Toko yang tidak beroprasi adalah toko " + toko);
+  toko++;
+}
