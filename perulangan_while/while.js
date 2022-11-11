d15de013a34e8b919bb985b3cdd332ea177d35cf
damosdmahdi <godrihidayat2@gmail.com>
2022-11-11 07:39:48 +0700

    perulangan While

---
+++ perulangan_while/while.js
@@ -0,0 +1,8 @@
+var Toko = true;
+var jumalahToko = 1;
+
+while ((Toko <= 5, jumalahToko <= 5)) {
+  console.log("Penujalan hari ini di toko " + jumalahToko + " aman terkendali");
+  Toko++;
+  jumalahToko++;
+}
